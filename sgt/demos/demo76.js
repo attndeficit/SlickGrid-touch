2aec7c5e4727d5a911cf3f29efb59ef7f5fbfdfd
Balazs Ree <ree@greenfinity.hu>
2012-10-07 11:13:02 +0200

    Factor out the minimum that we need from jquery-ui (the keycodes definitions) into a separate resource.

--- sgt/demos/demo76.js
+++ sgt/demos/demo76.js
@@ -9,38 +9,6 @@
 
 (function ($) {
 
-    // Proxy a minimal but necessary part of jquery-ui
-    $.ui = $.ui || {};
-    if (! $.ui.version) {
-        $.extend($.ui, {
-            keyCode: {
-                BACKSPACE: 8,
-                COMMA: 188,
-                DELETE: 46,
-                DOWN: 40,
-                END: 35,
-                ENTER: 13,
-                ESCAPE: 27,
-                HOME: 36,
-                LEFT: 37,
-                NUMPAD_ADD: 107,
-                NUMPAD_DECIMAL: 110,
-                NUMPAD_DIVIDE: 111,
-                NUMPAD_ENTER: 108,
-                NUMPAD_MULTIPLY: 106,
-                NUMPAD_SUBTRACT: 109,
-                PAGE_DOWN: 34,
-                PAGE_UP: 33,
-                PERIOD: 190,
-                RIGHT: 39,
-                SPACE: 32,
-                TAB: 9,
-                UP: 38
-            }
-        });
-    }
-
-
     function requiredFieldValidator(value) {
         if (value === null || value === undefined || !value.length) {
             return {valid: false, msg: "This is a required field"};
---
+++ sgt/lib/jquery.ui.keycodes.js
@@ -0,0 +1,41 @@
+
+/*jslint undef: true, newcap: true, nomen: false, white: true, regexp: true */
+/*jslint plusplus: false, bitwise: true, maxerr: 50, maxlen: 110, indent: 4 */
+/*jslint sub: true */
+/*globals window navigator document */
+/*globals setTimeout clearTimeout setInterval */ 
+
+(function ($) {
+
+    // Proxy a minimal but necessary part of jquery-ui
+    $.ui = $.ui || {};
+    if (! $.ui.version) {
+        $.extend($.ui, {
+            keyCode: {
+                BACKSPACE: 8,
+                COMMA: 188,
+                DELETE: 46,
+                DOWN: 40,
+                END: 35,
+                ENTER: 13,
+                ESCAPE: 27,
+                HOME: 36,
+                LEFT: 37,
+                NUMPAD_ADD: 107,
+                NUMPAD_DECIMAL: 110,
+                NUMPAD_DIVIDE: 111,
+                NUMPAD_ENTER: 108,
+                NUMPAD_MULTIPLY: 106,
+                NUMPAD_SUBTRACT: 109,
+                PAGE_DOWN: 34,
+                PAGE_UP: 33,
+                PERIOD: 190,
+                RIGHT: 39,
+                SPACE: 32,
+                TAB: 9,
+                UP: 38
+            }
+        });
+    }
+
+})(window.jQuery);
